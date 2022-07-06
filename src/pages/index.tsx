8f58445d56e1e183e96e36426cf76105960ccd4d
Mairon Vilela <maironvilela@hotmail.com>
2022-07-06 11:07:46 -0300

    :heavy_plus_sign: chore: add SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next';
- 
 
-  const Home: NextPage = () => {
-  return (
-      <h1>Home</h1>
-  )}
+const Home: NextPage = () => {
+  return <h1>Home</h1>;
+};
 
-  export default Home;
+export default Home;
